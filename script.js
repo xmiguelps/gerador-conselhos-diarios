7c7062955a0d98aa3375f25a535c71175065330e
Miguel Passos Gibertoni de Souza Alvez <gibertoni.passos@gmail.com>
2025-09-24 21:38:44 -0300

    feat: Adiciona interatividade ao botão de gerar conselho

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ const conselhos = [
     "Seja gentil com todos que encontrar."
 
 ]
-console.log("Arquivo de script carregado. Array de conselhos pronto")
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho() {
+    //BUG INTENCIONAL AQUI
+    const indice = Math.floor(Math.random() * (conselhos.length + 1));
+    conselhoTexto.textContent = conselhos[indice]
+}
+novoConselhoBtn.addEventListener('click', gerarConselho);
