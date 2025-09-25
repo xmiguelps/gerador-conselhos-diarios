b22b90d510b6916458c6fc5e841216074be24d73
Miguel Passos Gibertoni de Souza Alvez <gibertoni.passos@gmail.com>
2025-09-24 21:21:02 -0300

    Criação de upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,9 @@
+const conselhos = [
+    "Acredite em você mesmo.",
+    "O primeiro passo é o mais importante.",
+    "A persistencia realiza o impossivel.", 
+    "Aprenda algo novo hoje.",
+    "Seja gentil com todos que encontrar."
+
+]
+console.log("Arquivo de script carregado. Array de conselhos pronto")
