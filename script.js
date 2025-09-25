fd683f9920ccda12c1a177c88fd14c247e5edfd6
Miguel Passos Gibertoni de Souza Alvez <gibertoni.passos@gmail.com>
2025-09-24 21:55:30 -0300

    fix: Corrigi o calculo do indice e reativa a funcionalidade

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ const conselhos = [
     "Seja gentil com todos que encontrar."
 
 ]
-console.log("Arquivo de script carregado. Array de conselhos pronto")
+
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho() {
+    const indice = Math.floor(Math.random() * (conselhos.length));
+    conselhoTexto.textContent = conselhos[indice]
+}
+novoConselhoBtn.addEventListener('click', gerarConselho);
